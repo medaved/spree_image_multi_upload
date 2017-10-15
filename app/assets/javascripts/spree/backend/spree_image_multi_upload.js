c31ab52b8f17ff7e2d1f607b8e316b1583f8db67
DmitryEvgenevich <medavedik@gmail.com>
2017-10-16 00:32:36 +0300

    omg, fix load assets js

--- app/assets/javascripts/spree/backend/spree_image_multi_upload.js
+++ app/assets/javascripts/spree/backend/spree_image_multi_upload.js
@@ -1,2 +1,9 @@
-//= require spree/backend
-//= require_tree .
+//= require ./jquery-fileupload/vendor/jquery.ui.widget
+//= require ./jquery-fileupload/vendor/load-image
+//= require ./jquery-fileupload/vendor/canvas-to-blob
+//= require ./jquery-fileupload/vendor/tmpl
+//= require ./jquery-fileupload/jquery.iframe-transport
+//= require ./jquery-fileupload/jquery.fileupload
+//= require ./jquery-fileupload/jquery.fileupload-fp
+//= require ./jquery-fileupload/jquery.fileupload-ui
+//= require ./jquery-fileupload/locale
