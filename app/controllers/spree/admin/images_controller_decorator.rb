ac0b88d83e069830008ba5640a671da6d5abd1e6
DmitryEvgenevich <medavedik@gmail.com>
2017-10-15 23:35:43 +0300

    compatibility with spree 3.x(tested 3.2)

--- app/controllers/spree/admin/images_controller_decorator.rb
+++ app/controllers/spree/admin/images_controller_decorator.rb
@@ -1,15 +1,32 @@
 Spree::Admin::ImagesController.class_eval do
   def create
-    product = Spree::Product.where('slug' => params['product_id'])
     params[:image][:attachment].each do |attachment_object|
       image = Spree::Image.new
       image.attachment = attachment_object
       image.viewable_id = params[:image][:viewable_id] # product[0].id
       image.type = 'Spree::Image'
       image.viewable_type = 'Spree::Variant'
-      image.save
+      if image.save
+        result = {
+          files: [
+            image.to_jq_upload.merge(
+              # TODO: this is a dirty hack. See model decorator TODO for details
+              edit_url: edit_admin_product_image_url(image.viewable.product, image),
+              delete_url: admin_product_image_url(image.viewable.product, image)
+            )
+          ]
+        }
+        respond_to do |format|
+          format.html { render json: result, content_type: 'text/html', layout: false }
+          format.json { render json: result, status: :created }
+        end
+      else
+        respond_to do |format|
+          format.html { render action: 'index' }
+          format.json { render json: image.errors, status: :unprocessable_entity, layout: false }
+        end
+      end
     end
-    redirect_to '/admin/products/' + product[0].slug + '/images' # admin_product_url(product)
   end
 
   def multi_upload; end
--- lib/generators/spree_image_multi_upload/install/install_generator.rb
+++ lib/generators/spree_image_multi_upload/install/install_generator.rb
@@ -1,7 +1,6 @@
 module SpreeImageMultiUpload
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_image_multi_upload\n"
       end
@@ -9,7 +8,6 @@ module SpreeImageMultiUpload
       def add_stylesheets
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_image_multi_upload\n", :before => /\*\//, :verbose => true
       end
-
     end
   end
 end
